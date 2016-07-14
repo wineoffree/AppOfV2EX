979b70ad9649c9b5903ddcc5fc45d953bad71a83
wineoffree <1872774470@qq.com>
2016-07-14 19:38:19 +0800

    Add:增加了搜索功能

---
+++ Moudle9/app/src/main/java/com/example/administrator/appofv2ex/Date.java
@@ -0,0 +1,57 @@
+package com.example.administrator.appofv2ex;
+
+import java.io.Serializable;
+
+/**
+ * 用来存放界面一到界面二内容的类
+ * Created by Administrator on 2016/7/12.
+ */
+public class Date  implements Serializable{
+    private String newUrl;
+    private String name;
+    private String type;
+    private String showId;
+    private String time;
+    private String content;
+    private int index;
+    private String url;
+    //private Bitmap ima;
+    public Date(String newUrl,String name,String type,String showId,String time,String content,int index,String url){
+        this.newUrl=newUrl;
+        this.name=name;
+        this.type=type;
+        this.showId=showId;
+        this.time=time;
+        this.content=content;
+        this.index=index;
+        this.url=url;
+        //this.ima=bitmap;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShowId() {
+        return showId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    public int getIndex(){return index;}
+    public String getNewUrl(){return newUrl;}
+}
+
---
+++ Moudle9/app/src/main/java/com/example/administrator/appofv2ex/netspider/SecondTask.java
@@ -0,0 +1,123 @@
+package com.example.administrator.appofv2ex.netspider;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.administrator.appofv2ex.MainActivity;
+import com.example.administrator.appofv2ex.R;
+import com.example.administrator.appofv2ex.SecondActivity;
+import com.example.administrator.appofv2ex.downloadimg.DownImageTask;
+import com.example.administrator.appofv2ex.save.CacheImage;
+import com.example.administrator.appofv2ex.save.DateFromFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/7/11.
+ * 对第二层爬取
+ */
+public class SecondTask extends AsyncTask<String, Integer, List<Map<String,String>>> {
+    ProgressDialog progressDialog;
+    LinearLayout linearLayout;
+    Context context;
+    int i;
+    String url;
+    String topicContent;
+    TextView textView;
+    DateFromFile dateFromFile;
+    int index;
+    String imaUrl;
+    ImageView imageView;
+    Bitmap bitmap;
+    boolean ifHasNet;
+   ArrayList<View>views=new ArrayList<View>();
+    public SecondTask(ProgressDialog progressDialog ,
+                      Context context,
+                      String url,
+                      TextView textView,
+                      LinearLayout linearLayout,
+                      int index,
+                      String imaUrl,
+                      ImageView imageView,
+                      boolean ifHasNet) {
+        this.progressDialog=progressDialog;
+        this.context=context;
+        this.url=url;
+        this.textView=textView;
+        this.linearLayout=linearLayout;
+        this.index=index;
+        this.imaUrl=imaUrl;
+        this.imageView=imageView;
+        this.ifHasNet=ifHasNet;
+    }
+    @Override
+    protected void onPreExecute() {
+        // TODO Auto-generated method stub
+        super.onPreExecute();
+        progressDialog.show();
+    }
+
+    @Override
+    protected void onPostExecute(List<Map<String, String>> result) {
+        // TODO Auto-generated method stub
+
+        super.onPostExecute(result);
+        imageView.setImageBitmap(bitmap);
+        Log.d("hahaha","show:615156");
+        if(ifHasNet) {
+            textView.setText(Html.fromHtml(topicContent));
+            textView.setMovementMethod(LinkMovementMethod.getInstance());
+            LinearLayout layout = new LinearLayout(context);
+            LayoutInflater inflater = LayoutInflater.from(context);
+            for (int i = 0; i < result.size(); i++) {
+                View view = inflater.inflate(R.layout.layout_item, null);
+                TextView content = (TextView) (view.findViewById(R.id.content));
+                content.setText(result.get(i).get("content").toString());
+                TextView time = (TextView) (view.findViewById(R.id.time));
+                time.setText(result.get(i).get("time").toString());
+                TextView name = (TextView) (view.findViewById(R.id.name));
+                name.setText(result.get(i).get("name").toString());
+                ImageView img = (ImageView) (view.findViewById(R.id.ima));
+                new DownImageTask(img, false, 0).execute(result.get(i).get("ima"));
+                linearLayout.addView(view);
+            }
+        }
+
+
+        progressDialog.dismiss();
+    }
+
+    @Override
+    protected List<Map<String,String>> doInBackground(String... params) {
+        //通过内存找
+        Bitmap abitmap = CacheImage.getBitmap(imaUrl.replaceAll("/","").replace(".png?m=","").replace("_",""));
+        if(abitmap!=null)
+        this.bitmap=abitmap;
+        else {//通过文件找
+            this.bitmap= BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getAbsolutePath()+"/myv2ex/"+imaUrl.replaceAll("/","").replace(".png?m=","").replace("_","")+".png");
+        }
+        List<Map<String,String>> list=null;
+        if(ifHasNet){
+        DrawDate drawDate=new DrawDate(url);
+        drawDate.connectSecond();
+        topicContent= drawDate.getTopicContent();
+        list=drawDate.getSecondRequestList();}
+        return list;
+    }
+
+}
